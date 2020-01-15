27f24a33a7d5f9c8bcbc0cb4b9e308a0c5167b0c
Hugo Davila <hddavila@espol.edu.ec>
2020-01-14 23:43:54 -0500

    Se modifico la ventana Inicio con un metodo que muestra la ventana configuracion

--- LoteriaMexicana/src/ventanas/VntConfiguracion.java
+++ LoteriaMexicana/src/ventanas/VntConfiguracion.java
@@ -5,8 +5,11 @@
  */
 package ventanas;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 /**
  *
@@ -32,7 +35,24 @@ public class VntConfiguracion {
     }
     
     public void createContent(){
+// 
+//        root.setTop(new Label("Configuracion"));
+//        
+//        HBox fila1=new HBox(10);
+//        fila1.setStyle("-fx-background-color: #B3F6F3;");
+//        Label lblCantidad=new Label("Cantidad de oponentes");
+//        cantidad=new ComboBox();
+//        cantidad.getItems().addAll(0,1,2);
+//        
+//        fila1.getChildren().addAll(lblCantidad,cantidad);
+//        fila1.setAlignment(Pos.CENTER);
+//        
+//        
+//        HBox fila2=new HBox(10);
+//        
+//        root.setCenter(fila1);
         
+
     }
     
 }
