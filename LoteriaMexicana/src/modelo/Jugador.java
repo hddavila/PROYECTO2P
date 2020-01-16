125b7f41acd14ebc6f1238051d4ff1b3d2b262e1
Gabriel Castro <gacagonz@espol.edu.ec>
2020-01-16 00:18:14 -0500

    inicializacion de clases necesarias

---
+++ LoteriaMexicana/src/modelo/Jugador.java
@@ -0,0 +1,12 @@
+
+package modelo;
+
+/**
+ *
+ * @author Gabriel Castro
+ */
+public class Jugador {
+    private String nombre;
+    private Tablero tablero;
+    
+}
---
+++ LoteriaMexicana/src/modelo/Tablero.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Usuario
+ */
+class Tablero {
+    private ArrayList<Carta> cartas_Jug;
+    private ArrayList<Carta> anunciadas;
+    
+    
+    
+}
