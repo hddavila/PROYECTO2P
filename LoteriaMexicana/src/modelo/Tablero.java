f8a04d7e3591adf52c7c4ee88651a897eb1c0730
Gabriel Castro <gacagonz@espol.edu.ec>
2020-01-16 19:56:16 -0500

    nuevo fondo para la pantalla

--- LoteriaMexicana/src/modelo/Tablero.java
+++ LoteriaMexicana/src/modelo/Tablero.java
@@ -2,6 +2,7 @@
 package modelo;
 
 import java.util.ArrayList;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 /**
@@ -26,9 +27,23 @@ class Tablero {
     public void crearTablero(){
         for(int column = 0; column < 4; column++){
             for(int row = 0; row < 4; row++){
-                root.add(cartas_Jug.get(row+column).getImagen(),column, row);
+                ImageView carta;
+                carta = cartas_Jug.get(row+column).getImagen();
+                 
+                //Funcion al clicar la carta
+                carta.setOnMouseClicked(e->{
+                    
+                    
+                });
+                root.add(carta,column, row);
+                
             }
         }
+    }
+    
+    public void anunciar(){
+        
+        
     }
 
     public GridPane getRoot() {
