48d7f71585ae414a921a03c10f8acdf9441478e6
Gabriel Castro <gacagonz@espol.edu.ec>
2020-01-18 16:01:04 -0500

    Cronometro implementado

---
+++ LoteriaMexicana/src/modelo/Cronometro.java
@@ -0,0 +1,65 @@
+
+package modelo;
+
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Gabriel 
+ */
+public class Cronometro extends Thread {
+    private Thread hilo;
+    private boolean activo;
+    private Label show;
+    private double now;
+    
+    public Cronometro(){
+        this.activo = false;
+        show = new Label(Double.toString(now));
+        this.now = 3; 
+    }
+    
+    public void inciar(){
+        this.activo = true;
+        hilo = new Thread(this);
+        hilo.start();
+    }
+    
+    public void parar(){
+        this.activo  = false;
+    }
+    
+    @Override
+    public void run(){
+        
+        try{
+            while(activo){
+                
+                hilo.sleep(100);
+                now -= 0.1;
+                if (now == 2.0){
+                    show.setText(Double.toString(now));
+                }
+                else if(now == 1.0){
+                    show.setText(Double.toString(now));
+                }
+                else if(now == 0.0){
+                    show.setText(Double.toString(now));
+                }
+            }
+        }
+        catch(Exception e){
+            
+        }
+        
+    }
+    
+    public double getTime(){
+        return this.now;
+    }
+    
+    public Label getShow(){
+        return this.show;
+    }
+    
+}
