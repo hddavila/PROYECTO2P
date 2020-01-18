217a3b3bd04fc6ffa3bbf81fdc8aa66a0b75b822
Gabriel Castro <gacagonz@espol.edu.ec>
2020-01-18 16:15:18 -0500

    Clase cronometro comentada

--- LoteriaMexicana/src/modelo/Cronometro.java
+++ LoteriaMexicana/src/modelo/Cronometro.java
@@ -13,22 +13,54 @@ public class Cronometro extends Thread {
     private Label show;
     private double now;
     
+    
+/**
+ *
+ * Constructor que setea el cronometro como inactivo, inicia en 3 y 
+ * crea la Label para mostrar el tiempo;
+ */
     public Cronometro(){
         this.activo = false;
         this.now = 3; 
         show = new Label(Double.toString(now));
     }
     
+ /**
+ *
+ * Funcion para iniciar el cronometro, cambia el estado a activo, crea el hilo
+ * y lo inicia.
+ */
+    
     public void inciar(){
         this.activo = true;
         hilo = new Thread(this);
         hilo.start();
     }
+ /**
+ *
+ * Funcion para parar el cronometro, cambia el estado a inactivo y detienen el
+ * hilo.
+ */
     
     public void parar(){
         this.activo  = false;
+        hilo.stop();
+    }
+/**
+ *
+ * Funcion para reiniciar el cronometro, settea el tiempo en 3
+ */
+    public void reiniciar(){
+        this.now = 3;
     }
     
+/**
+ *
+ * Metodo run sobreescrito.
+ * Mientras el estado del cronometro sea activo manda al hilo a dormir por 0.1 s
+ * y verifica el tiempo.
+ * Si el tiempo es 2 cambia el label a 2, si es 1 a 1 y si es 0 a 0.
+ */
     @Override
     public void run(){
         
@@ -38,13 +70,13 @@ public class Cronometro extends Thread {
                 hilo.sleep(100);
                 now -= 0.1;
                 if (now == 2.0){
-                    show.setText(Double.toString(now));
+                    show = new Label(Double.toString(now));
                 }
                 else if(now == 1.0){
-                    show.setText(Double.toString(now));
+                    show = new Label(Double.toString(now));
                 }
                 else if(now == 0.0){
-                    show.setText(Double.toString(now));
+                    show = new Label(Double.toString(now));
                 }
             }
         }
@@ -57,7 +89,7 @@ public class Cronometro extends Thread {
     public double getTime(){
         return this.now;
     }
-    
+
     public Label getShow(){
         return this.show;
     }
